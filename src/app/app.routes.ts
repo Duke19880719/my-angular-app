9a122ce8de8d6e71533ea7ab776282bb1ca5ffdd
Allen Chiang <wolfsky95@gmail.com>
2025-10-04 17:37:20 +0800

    修改ROUTES 回 LOGIN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { RecordEdit } from './record-edit/record-edit';
 import { RecordList } from './record-list/record-list';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },//測試用，要記得改回 login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },//測試用，要記得改回 login
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'record-add', component: RecordAdd },
